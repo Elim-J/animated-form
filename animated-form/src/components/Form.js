34fd0ef79320514ddd53d2d19bf7ee3d71f1fa32
skramer2329 <skramer2329@gmail.com>
2022-06-25 18:57:23 -0500

    Added css, radio, check, and text boxes

--- animated-form/src/components/Form.js
+++ animated-form/src/components/Form.js
@@ -6,7 +6,10 @@ export default function Form(){
         firstName: "",
         lastName: "",
         email: "",
-        userName: ""
+        userName: "",
+        comments: "",
+        knowReact: false,
+        languages: ""
     })
 
     console.log(formData)
@@ -14,9 +17,11 @@ export default function Form(){
     function updateForm(event){
         
         setFormData(prevFormData => {
+
+            const {name, value, type, checked} = event.target
             return {
                 ...prevFormData,
-                [event.target.name]: event.target.value
+                [name]: type === "checkbox" ? checked : value
             }
         })
     }
@@ -29,6 +34,7 @@ export default function Form(){
                     placeholder="First Name"
                     name="firstName"
                     onChange={updateForm}
+                    value={formData.firstName}
                 />
 
                 <input 
@@ -36,13 +42,15 @@ export default function Form(){
                     placeholder="Last Name"
                     name="lastName"
                     onChange={updateForm}
+                    value={formData.lastName}
                 />
 
                 <input 
-                    type="text" 
+                    type="email" 
                     placeholder="Email"
                     name="email"
                     onChange={updateForm}
+                    value={formData.email}
                 />
 
                 <input 
@@ -50,7 +58,66 @@ export default function Form(){
                     placeholder="Username"
                     name="userName"
                     onChange={updateForm}
+                    value={formData.userName}
                 />
+
+                <textarea 
+                    placeholder="Add Comments Here"
+                    name="comments"
+                    value={formData.comments}
+                    onChange={updateForm}
+                />
+
+                <input 
+                    type="checkbox"
+                    id="knowReact"
+                    checked={formData.knowReact}
+                    onChange={updateForm}
+                    name="knowReact"
+                />
+                <label htmlFor="knowReact">Do you know React?</label>
+                <br />
+                <br />
+                <fieldset>
+                    <legend>How many programming languages do you know?</legend>
+                    <input 
+                        type="radio"
+                        id="none"
+                        checked={formData.languages === "none"}
+                        onChange={updateForm}
+                        name="languages"
+                        value="none"
+                    />
+                    <label htmlFor="none">None</label>
+
+                    <br />
+
+                    <input 
+                        type="radio"
+                        id="one"
+                        value="one"
+                        name="languages"
+                        onChange={updateForm}
+                        checked={formData.languages === "one"}
+
+                    />
+                    <label htmlFor="one">One</label>
+
+                    <br />
+
+                    <input 
+                        type="radio"
+                        id="more"
+                        name="languages"
+                        value="Too many to count"
+                        onChange={updateForm}
+                        checked={formData.languages === "Too many to count"}
+                    />
+
+                    <label htmlFor="more">Too many to count</label>
+
+                    <br />
+                </fieldset>
             </form>
         </div>
     )
