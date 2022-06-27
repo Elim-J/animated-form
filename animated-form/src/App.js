25fa54284d171709a305567de9fc4b0157abe51e
skramer2329 <skramer2329@gmail.com>
2022-06-27 15:16:10 -0500

    Changes to App.js, Header.js, Header.css

--- animated-form/src/App.js
+++ animated-form/src/App.js
@@ -1,11 +1,22 @@
 import "./styles/App.css"
+import React from "react"
 import Form from "./components/Form"
 import Header from "./components/Header"
 
 function App() {
+  
+  const [darkMode, setDarkMode] = React.useState(true)
+
+  function chooseMode(){
+    setDarkMode(prevMode => !prevMode)
+  }
+
   return (
     <div className="App">
-      <Header />
+      <Header 
+        darkMode={darkMode}
+        handleMode={chooseMode}
+      />
       <Form />
     </div>
   );
--- animated-form/src/components/Header.js
+++ animated-form/src/components/Header.js
@@ -1,10 +1,23 @@
 import React from "react"
 import "../styles/Header.css"
 
-export default function Header(){
+export default function Header(props){
     return (
-        <div>
-            <p>This is Header body</p>
-        </div>
+        <nav
+            className={props.darkMode ? "dark" : ""}
+        >
+            <div
+                className="toggler"
+            >
+                <p className="toggler--light">Light</p>
+                <div
+                    className="toggler--slider"
+                    onClick={props.handleMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
     )
 }
